d8df8bfa3d094fd02dab53d4de7b928c3f81bedd
Peter Mescalchin <peter@magnetikonline.com>
2016-04-12 22:36:17 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,152 @@
+# Bash web application deployer
+A web application build & deployment script written in pure Bash, with per-project setup controlled by a simple configuration file.
+
+- [Features](#features)
+- [Requirements](#requirements)
+- [Configuration](#configuration)
+	- [Source application](#source-application)
+		- [Settings](#settings)
+		- [Pre-deploy hook](#pre-deploy-hook)
+	- [Target server](#target-server)
+- [Deploying](#deploying)
+
+## Features
+Script is rather opinionated in what is provided - fitting preferences for components/technologies/tools used with web applications I'm actively developing - it might prove useful for a few developers, or possibly no-one:
+
+- Deployment of source files to target server controlled by a simple include/exclude path filter list.
+- Compilation of one or more [Sass](http://sass-lang.com/) style sheets to build target with automated minification.
+- Concatenation and minification of one or more source JavaScript file(s) to build target.
+- Automatic creation and deployment of gzipped CSS/JavaScript resources alongside uncompressed/default versions for use with Nginx [`ngx_http_gzip_static_module` module](http://nginx.org/en/docs/http/ngx_http_gzip_static_module.html) all at the maximum compression level offered by `gzip`.
+- An optional pre-deployment hook bash function to apply any final programmatic changes to built application, such as [Git SHA1 based build versioning](#pre-deploy-hook).
+- Rsync over SSH based deployments from source to target.
+
+## Requirements
+- Deployment system of Debian/Ubuntu Linux or variant.
+- [Rsync](https://download.samba.org/pub/rsync/rsync.html) installed on both deployment source and target systems.
+- [Sass](http://sass-lang.com) for compilation of source `.scss` style sheets to `.css`. Using original Ruby based version.
+- [Sass Globbing plugin](https://github.com/chriseppstein/sass-globbing) Ruby extension for Sass - allowing for wildcard `@include` of Sass documents.
+- [YUI Compressor](http://yui.github.io/yuicompressor/) and [Google Closure Compiler](https://developers.google.com/closure/compiler/) for CSS and JavaScript minification tasks respectively.
+- Java CLI required by both YUI Compressor and Google Closure Compiler.
+- Working SSH access to target system via public/private keys.
+
+## Configuration
+Setup of script is in two parts - the source application and target server.
+
+### Source application
+It's assumed the script will be placed at a central location to be shared, via a symlink placed in each web application's source repository. For example:
+
+```sh
+$ git clone https://github.com/magnetikonline/bashwebappdeploy.git /target/path/for/bashwebappdeploy
+$ cd /path/to/my/web/application
+$ ln -s /target/path/for/bashwebappdeploy/deploy.sh ./script
+$ git add ./script/deploy.sh
+$ git commit -m 'Added symlink for bashwebappdeploy'
+```
+
+Configuration is via `deploy.config` file(s) which will be loaded from either (in the following order):
+- Inline with created symlink to `deploy.sh` (from above example, this would be `./script/deploy.sh`), or...
+- Inline with the full canonical path to `deploy.sh`.
+
+This allows for global (system) plus per-application configuration settings, which will be combined.
+
+#### Settings
+- `JAR_YUI_COMPRESSOR` - Location of [YUI Compressor](http://yui.github.io/yuicompressor/) jar archive on source system.
+- `JAR_GOOGLE_CLOSURE` - Location of [Google Closure Compiler](https://developers.google.com/closure/compiler/) jar archive on source system.
+- `SOURCE_DIR` - Application source, relative to `deploy.sh` (or called symlink to it). For example if `deploy.sh` symlink and `deploy.config` are located under a `/script/` directory, `SOURCE_DIR` could simply be given as `"../"` to reference the application source root.
+- `BUILD_SOURCE_FILTER` - Include/exclude rules for directories and files to be added from application source to the build. Specified via Rsync format filter rules - refer to `FILTER RULES` at the [Rsync man page](https://download.samba.org/pub/rsync/rsync.html) for filter rule format/usage.
+- `BUILD_SASS_LIST` - Sass style sheets for compilation to CSS, minified and gzip version created during the build process.
+
+	Each line item given as a SCSS document source path and target CSS document path within the build target, separated via a pipe (`|`) character. Example:
+
+	```
+	/public/css/style.scss|/public/css/style.css
+	/assets/scss/themes.scss|/docroot/themes.css
+	```
+
+- `BUILD_JAVASCRIPT_LIST` - JavaScript source files to be (optionally) combined, minified and gzip version created during the build process.
+
+	Each JavaScript file is specified as one or more code paths from the application source - comma (`,`) separated and followed by a target JavaScript source path within the build target, separated via a pipe (`|`) character. Source JavaScript code paths in addition supports [globs](https://en.wikipedia.org/wiki/Glob_(programming)). Example:
+
+	```
+	/public/js/lib/picoh.js,/public/js/app/*.js|/public/js/app.js
+	/src/javascript/page.js|/docroot/page.js
+	```
+
+- `SERVER_HOSTNAME` - Target SSH server where application will be deployed to.
+- `SERVER_SSH_USER` - SSH deployment username.
+- `SERVER_SSH_PORT` - Deployment target SSH listening port (typically will be `22`).
+- `SERVER_RSYNC_MODULE` - Receiving Rsync module name at target server (see [Target server](#target-server) section below).
+- `SERVER_RSYNC_CHMOD` - The permissions Rsync will set on directories/files transfered to target, using the syntax of Rsync's `--chmod` argument.
+
+	For example to set:
+	* Read/execute permissions for _all directories_.
+	* Read permissions for _all files_.
+	* Write permissions for _both_ directories/files for the *owner only*.
+
+	```
+	a-rwx,Da+rx,Fa+r,u+w
+	```
+
+- `SERVER_EXCLUDE_FILTER` - Exclude rules for directories and files on target server that will not be considered. Useful for items such as logs or application generated files (e.g. caches), ensuring Rsync transfer does not clean-up/delete given paths. Specified via a simplified form of Rsync filter rules - refer to `FILTER RULES` at the [Rsync man page](https://download.samba.org/pub/rsync/rsync.html).
+
+#### Pre-deploy hook
+In addition to above settings a bash function of `preDeployHook` can be defined, which will be called just prior to Rsync deployment of the generated build to the target server - allowing for any final custom/programmatic modifications. The function is passed arguments of the source application root and build target directories.
+
+As an example, to apply the current application source Git SHA1 to placeholder(s) within a HTML file for use as a build identifier:
+
+```sh
+function preDeployHook {
+
+	# fetch current Git revision SHA1
+	local gitSHA1=$(git --git-dir "$1/.git" rev-parse HEAD)
+
+	# apply SHA1 to /index.html at token location of [BUILD_SHA1]
+	# note: only using first 16 characters of SHA1
+	local sedSearch="\[BUILD_SHA1\]"
+	local sedReplace="${gitSHA1:0:16}"
+	sed -i -r "s/$sedSearch/$sedReplace/" "$2/index.html"
+}
+```
+
+The provided [deploy.config](example/script/deploy.config) example should help understand all these settings and their usage together.
+
+### Target server
+Configuration of target server involves the creation of an Rsync module to receive the built application files and place them in the desired web root/directory.
+
+The following example would exist in the home folder of our `SERVER_SSH_USER` named `~/rsyncd.conf` and provides:
+- Two modules named `site-domain.com` and `site-anotherdomain.com` respectively.
+- Read and write permissions to both modules.
+- Fairly detailed logging of all transfer activity to a `~/rsyncd.log` file.
+
+```
+list = false
+log file = rsyncd.log
+log format = %i %f [%l]
+read only = false
+transfer logging = true
+use chroot = false
+
+
+[site-domain.com]
+path = /srv/http/domain.com
+
+[site-anotherdomain.com]
+path = /srv/http/anotherdomain.com
+```
+
+## Deploying
+Starting a deployment is as easy as running `./deploy.sh`. The script will generate the application build in a temporary directory as defined from source - then push the result to the target server via Rsync over SSH.
+
+A few optional command line switches are provided:
+
+```
+Usage: deploy.sh [OPTION]...
+
+  -d    execute a dry-run rsync deployment
+  -t    retain build temporary directory for
+        review or verification after deployment
+  -h    display help
+```
+
+- The `-d` switch will complete all steps, but _simulate_ the Rsync deployment step by using Rsync's `--dry-run` mode.
+- To complete a build/deploy process, but retain the temporary directory of built application, provide the `-t` switch - handy to couple with `-d` while authoring a new deployment configuration.
---
+++ deploy.sh
@@ -0,0 +1,344 @@
+#!/bin/bash -e
+
+DIRNAME=$(dirname "$0")
+DEPLOY_CONFIG_FILE="deploy.config"
+MKTEMP_TEMPLATE="deploybuild.XXXXX"
+
+
+function exitError {
+
+	echo "Error: $1" >&2
+	exit 1
+}
+
+function writeWarning {
+
+	echo "Warning: $1"
+}
+
+function writeNotice {
+
+	echo "Notice: $1"
+}
+
+function usage {
+
+	cat <<EOM
+Usage: $(basename $0) [OPTION]...
+
+  -d    execute a dry-run rsync deployment
+  -t    retain build temporary directory for
+        review or verification after deployment
+  -h    display help
+EOM
+
+	exit 2
+}
+
+function loadConfiguration {
+
+	# attempt to load a global configuration file inline with the canonical script path
+	# note: global config file may not exist, that's fine
+	local globalConfigFile=$(getPathCanonical "$0")
+	globalConfigFile="$(dirname "$globalConfigFile")/$DEPLOY_CONFIG_FILE"
+	[[ -f $globalConfigFile ]] && . "$globalConfigFile"
+
+	# now load application config file - must exist otherwise fatal error
+	local appConfigFile="$DIRNAME/$DEPLOY_CONFIG_FILE"
+	[[ -f $appConfigFile ]] || exitError "Unable to locate $appConfigFile."
+	. "$appConfigFile"
+}
+
+function getTempFile {
+
+	mktemp --tmpdir $MKTEMP_TEMPLATE
+}
+
+function getPathCanonical {
+
+	if [[ $2 == "nocheck" ]]; then
+		readlink -mn "$1"
+
+	else
+		# note: will return error = 1 if any path component non-exists except final - hence || true
+		readlink -fn "$1" || true
+	fi
+}
+
+function rsyncSourceToBuildDir {
+
+	# create temp file to hold source copy to build dir rsync filter rules
+	local filterTmp=$(getTempFile)
+	echo "$BUILD_SOURCE_FILTER" >"$filterTmp"
+
+	rsync -rt \
+		--out-format "%n" \
+		--filter ". $filterTmp" \
+		"$sourceDirCanonical/" "$buildDir"
+	echo
+
+	# delete temp file
+	rm -f $filterTmp
+}
+
+function gzipResource {
+
+	# source file exists?
+	[[ -f $1 ]] || return
+
+	# compress file and give identical timestamp to source (for Nginx ngx_http_gzip_static_module module)
+	echo "Compressing resource:"
+	echo "Source: $1"
+	echo "Target: $1.gz"
+	echo
+
+	gzip -c9 "$1" >"$1.gz"
+	touch -c --reference "$1" "$1.gz"
+}
+
+function buildSass {
+
+	# if no Sass files defined for build - exit
+	[[ -z $BUILD_SASS_LIST ]] && return
+
+	IFS=$'\n'
+	local sassBuildItem
+
+	for sassBuildItem in $BUILD_SASS_LIST; do
+		# ensure $sassBuildItem is in [SOURCE_SCSS|TARGET_CSS] format
+		if [[ $sassBuildItem =~ ^([^|]+)\|([^|]+)$ ]]; then
+			# fetch Sass source and CSS target parts canonicalized
+			local sassSource=$(getPathCanonical "$sourceDirCanonical/${BASH_REMATCH[1]}")
+			local cssTarget=$(getPathCanonical "$buildDir/${BASH_REMATCH[2]}" "nocheck")
+
+			# source Sass document exists?
+			if [[ -f $sassSource ]]; then
+				echo "Compiling/minifying Sass -> CSS:"
+				echo "Source: $sassSource"
+				echo "Target: $cssTarget"
+				echo
+
+				# create parent directory structure in build target for generated CSS document
+				mkdir -p $(dirname "$cssTarget")
+
+				# now compile Sass document to output CSS
+				sass \
+					--load-path "$(dirname "$sassSource")" \
+					--no-cache \
+					--require sass-globbing \
+					--scss \
+					--sourcemap=none \
+					"$sassSource" "$cssTarget"
+
+				# minify built CSS using YUI compressor then compress
+				cat "$cssTarget" | java -jar "$JAR_YUI_COMPRESSOR" --type css -o "$cssTarget"
+				gzipResource "$cssTarget"
+
+			else
+				writeWarning "Unable to locate $sassSource for CSS compile"
+				echo
+			fi
+
+		else
+			writeWarning "Invalid Sass build item definition: $sassBuildItem"
+			echo
+		fi
+	done
+
+	unset IFS
+}
+
+function buildJavaScript {
+
+	# if no JavaScript files defined for build - exit
+	[[ -z $BUILD_JAVASCRIPT_LIST ]] && return
+
+	IFS=$'\n'
+	local javaScriptBuildItem
+
+	for javaScriptBuildItem in $BUILD_JAVASCRIPT_LIST; do
+		unset IFS
+
+		# ensure $javaScriptBuildItem is in [SOURCE_JAVASCRIPT_LIST|TARGET_JAVASCRIPT] format
+		if [[ $javaScriptBuildItem =~ ^([^|]+)\|([^|]+)$ ]]; then
+			local javaScriptBuildTarget=$(getPathCanonical "$buildDir/${BASH_REMATCH[2]}" "nocheck")
+			local javaScriptTempConcatTarget=$(getTempFile)
+			local buildStepHeaderWritten=""
+
+			# work over each source JavaScript list item glob
+			IFS=","
+			local javaScriptSourceGlobItem
+
+			for javaScriptSourceGlobItem in ${BASH_REMATCH[1]}; do
+				unset IFS
+
+				# grab file(s) matched by source JavaScript source item
+				IFS=$'\n'
+				local javaScriptSourceFileItem
+				local globMatch=""
+
+				# note: don't quote [$sourceDirCanonical/$javaScriptSourceGlobItem] as may contain glob patterns
+				for javaScriptSourceFileItem in $sourceDirCanonical/$javaScriptSourceGlobItem; do
+					unset IFS
+
+					# get canonical path to source JavaScript file
+					javaScriptSourceFileItem=$(getPathCanonical "$javaScriptSourceFileItem")
+
+					if [[ -f $javaScriptSourceFileItem ]]; then
+						globMatch="true"
+						if [[ -z $buildStepHeaderWritten ]]; then
+							echo "Concatenate/minifying source JavaScript -> JavaScript:"
+							buildStepHeaderWritten="true"
+						fi
+
+						# add JavaScript to concatenate file target
+						echo "Source: $javaScriptSourceFileItem"
+						cat "$javaScriptSourceFileItem" >>"$javaScriptTempConcatTarget"
+					fi
+				done
+
+				if [[ -z $globMatch ]]; then
+					# resolved [$sourceDirCanonical/$javaScriptSourceGlobItem] glob path didn't match anything
+					writeWarning "JavaScript source glob $javaScriptSourceGlobItem did not match"
+				fi
+			done
+
+			if [[ -s $javaScriptTempConcatTarget ]]; then
+				# successfully concatenated one or more source JavaScript files to target file
+				echo "Target: $javaScriptBuildTarget"
+				echo
+
+				# create parent directory structure in build target for final JavaScript source file
+				mkdir -p "$(dirname "$javaScriptBuildTarget")"
+
+				# minify temporary JavaScript concatenate target to final build location then compress
+				cat "$javaScriptTempConcatTarget" | java -jar "$JAR_GOOGLE_CLOSURE" --js_output_file "$javaScriptBuildTarget"
+				gzipResource "$javaScriptBuildTarget"
+
+			else
+				writeWarning "Unable to create $javaScriptBuildTarget from source JavaScript file(s)"
+				echo
+			fi
+
+			# delete temporary JavaScript concatenate target
+			rm -f "$javaScriptTempConcatTarget"
+
+		else
+			writeWarning "Invalid JavaScript build item definition: $javaScriptBuildItem"
+			echo
+		fi
+	done
+
+	unset IFS
+}
+
+function SSHRsyncBuildDirToServer {
+
+	# create temp file to hold server exclude filter rules for rsync - empty file if not defined
+	local filterTmp=$(getTempFile)
+	[[ -n $SERVER_EXCLUDE_FILTER ]] && echo "$SERVER_EXCLUDE_FILTER" >"$filterTmp"
+
+	rsync -irtz \
+		--chmod "$SERVER_RSYNC_CHMOD" \
+		--delete \
+		${optionRsyncDryRunOnly:+--dry-run} \
+		--exclude-from "$filterTmp" \
+		--rsh "ssh -l $SERVER_SSH_USER -p $SERVER_SSH_PORT" \
+		"$buildDir/" "$SERVER_HOSTNAME::$SERVER_RSYNC_MODULE" || true
+
+	echo
+
+	# delete temp file
+	rm -f "$filterTmp"
+}
+
+
+# parse command line options
+optionRsyncDryRunOnly=""
+optionRetainBuildResultDir=""
+while getopts ":dth" optKey; do
+	case $optKey in
+		d)
+			optionRsyncDryRunOnly="true"
+			;;
+		t)
+			optionRetainBuildResultDir="true"
+			;;
+		h|*)
+			usage
+			;;
+	esac
+done
+
+# are rsync, java and sass installed?
+[[ -z $(which rsync) ]] && exitError "Unable to locate Rsync, installed?"
+[[ -z $(which java) ]] && exitError "Unable to locate Java, installed?"
+[[ -z $(which sass) ]] && exitError "Unable to locate Sass, installed?"
+
+# load (optional) global and application configuration
+loadConfiguration
+
+# validate config settings - YUI compressor/Google closure compiler jars
+[[ -z $JAR_YUI_COMPRESSOR ]] && exitError "JAR_YUI_COMPRESSOR config parameter not defined."
+[[ -f $JAR_YUI_COMPRESSOR ]] || exitError "Unable to locate YUI compressor jar at $JAR_YUI_COMPRESSOR."
+
+[[ -z $JAR_GOOGLE_CLOSURE ]] && exitError "JAR_GOOGLE_CLOSURE config parameter not defined."
+[[ -f $JAR_GOOGLE_CLOSURE ]] || exitError "Unable to locate Google Closure Compiler jar at $JAR_GOOGLE_CLOSURE."
+
+# validate config settings - application source directory
+[[ -z $SOURCE_DIR ]] && exitError "SOURCE_DIR config parameter not defined."
+
+# canonicalize $SOURCE_DIR and validate directory exists
+sourceDirCanonical=$(getPathCanonical "$DIRNAME/$SOURCE_DIR")
+[[ -d $sourceDirCanonical ]] || exitError "Unable to locate source directory at $DIRNAME/$SOURCE_DIR"
+
+# validate config settings - build source filter
+[[ -z $BUILD_SOURCE_FILTER ]] && exitError "BUILD_SOURCE_FILTER config parameter not defined."
+
+# validate config settings - target server details
+[[ -z $SERVER_HOSTNAME ]] && exitError "SERVER_HOSTNAME config parameter not defined."
+[[ -z $SERVER_SSH_USER ]] && exitError "SERVER_SSH_USER config parameter not defined."
+[[ -z $SERVER_SSH_PORT ]] && exitError "SERVER_SSH_PORT config parameter not defined."
+
+[[ -z $SERVER_RSYNC_MODULE ]] && exitError "SERVER_RSYNC_MODULE config parameter not defined."
+[[ -z $SERVER_RSYNC_CHMOD ]] && exitError "SERVER_RSYNC_CHMOD config parameter not defined."
+
+# everything validated - lets start the build process
+echo "Application source: $sourceDirCanonical"
+[[ -n $optionRsyncDryRunOnly ]] && writeNotice "Rsync dry-run deployment only"
+[[ -n $optionRetainBuildResultDir ]] && writeNotice "Retaining temporary build directory after deployment"
+
+# create build directory
+buildDir=$(mktemp -d --tmpdir $MKTEMP_TEMPLATE)
+echo "Build target: $buildDir"
+echo
+
+# rsync application source to build directory
+rsyncSourceToBuildDir
+
+# build Sass source files to minified/gzipped CSS
+buildSass
+
+# build JavaScript source files to bundled/minified/gzipped JavaScript
+buildJavaScript
+
+# if defined, run pre deployment hook function now
+if [[ $(type -t preDeployHook) == "function" ]]; then
+	preDeployHook "$sourceDirCanonical" "$buildDir"
+fi
+
+# rsync build directory to destination server via SSH transit
+SSHRsyncBuildDirToServer
+
+if [[ -n $optionRetainBuildResultDir ]]; then
+	# keeping build directory
+	echo "Build directory located at: $buildDir"
+
+else
+	# remove build directory
+	echo "Removing build target work area: $buildDir"
+	rm -rf $buildDir
+fi
+
+# success
+exit 0
